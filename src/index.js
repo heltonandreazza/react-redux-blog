7cba059a4b91decc206c4b9abc541842db18e098
Helton Andreazza <helton.prg@gmail.com>
2017-08-20 19:22:55 -0300

    create post list component; route; state and promise with axios

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ReduxPromise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/post_index';
